58f0e51e413e645b4c400f33c947ee688ab49a50
pawelchodzba123 <pawul0n@op.pl>
2021-01-03 23:01:54 +0100

    where.prperty abort, check constructor

---
+++ src/repository/query/where.js
@@ -0,0 +1,25 @@
+const { Op } = require("sequelize");
+
+ function Where(){
+       
+    const where  = {
+
+       }
+    
+    return {
+         and(){ 
+
+         }
+     }
+     
+    }
+
+
+ 
+
+
+module.exports = Where;
+
+
+
+
